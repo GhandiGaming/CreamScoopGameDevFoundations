02ed0246be025258493863f32aaaadca8c478b8a
Piphria <112468017+Piphria@users.noreply.github.com>
2022-10-06 17:14:48 +0930

    Update RandomSpawner.cs

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -10,22 +10,30 @@ public class RandomSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnNow", 5, 4);
+        InvokeRepeating("SpawnNow", 6f, 2.5f);
     }
 
     Vector3 getRandomPose()
     {
-        float _x = Random.Range(-40, 40);
+        float _x = Random.Range(40, 57);
         float _y = 0.5f;
-        float _z = Random.Range(-45, 46);
+        float _z = Random.Range(40, 57);
 
         Vector3 newPos = new Vector3(_x, _y, _z);
         return newPos;
     }
-
+    private void Update()
+    {
+        Invoke(nameof(StopSpawn), 90f);
+    }
     void SpawnNow()
     {
         Instantiate(enemiesToSpawn[Random.Range(0, 2)], getRandomPose(), Quaternion.identity);
+        Debug.Log("Enemy created");
     }
 
+    void StopSpawn()
+    {
+        CancelInvoke();
+    }
 }
