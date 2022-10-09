b347ab4fa715c72115f7390b8aacef4e39be9824
Piphria <112468017+Piphria@users.noreply.github.com>
2022-10-10 08:12:41 +0930

    winscreen to entering the elevator

---
+++ Assets/WinScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameWinScreen");
+
+        }
+    }
+}
